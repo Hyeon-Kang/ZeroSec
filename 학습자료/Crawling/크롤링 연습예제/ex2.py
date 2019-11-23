2dce95caba81c63cbb2b928474f19e2fee4e80fa
tragoni <52310656+tragoni@users.noreply.github.com>
2019-11-23 15:18:08 +0900

    Update ex2.py

--- 학습자료/Crawling/크롤링 연습예제/ex2.py
+++ 학습자료/Crawling/크롤링 연습예제/ex2.py
@@ -16,27 +16,26 @@ def crawler(url, parser, css_selector):
     datas = soup.select(css_selector)
 
     if parser == 'lxml':
-       print(datas[0].text, file=fileOut)
+        print(datas[0].text, file=fileOut)
     else:
-       for data in datas:
+        for data in datas:
             rsss.append(data.text)
 
-# 실행코드
-print("크롤링을 시작합니다.")
+            # 실행코드
+            print("크롤링을 시작합니다.")
 
-crawler('http://file.mk.co.kr/news/rss/rss_50300009.xml','xml','item link')
+            crawler('http://file.mk.co.kr/news/rss/rss_50300009.xml','xml','item link')
 
-print("rss 추출이 완료되었습니다.")
+            print("rss 추출이 완료되었습니다.")
 
-for link in rsss:
-    try:
-        crawler(link, 'lxml', '#article_body')
-        print("="*20)
-    except Exception as e:
-        print(e)
-        print('진행중이에요...')
-        continue
+            for link in rsss:
+                try:
+                    crawler(link, 'lxml', '#article_body')
+                    print("="*20)
+                except Exception as e:
+                    print(e)
+                    print('진행중이에요...')
+                    continue
 
-print("크롤링을 종료합니다.")
-fileOut.close()
-
+                    print("크롤링을 종료합니다.")
+                    fileOut.close()
