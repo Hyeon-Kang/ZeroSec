622a9589a3b487d41f9fab0fc29cc544f71c4d7b
Hyeon <rkd099@naver.com>
2019-11-26 10:48:37 +0900

    Create TextRank.py

---
+++ 학습자료/NLP/텍스트 유사도 비교/TextRank.py
@@ -0,0 +1,61 @@
+class TextRank(object):
+    def __init__(self, text):
+        self.sent_tokenize = SentenceTokenizer()
+
+        if text[:5] in ('http:', 'https'):
+            self.sentences = self.sent_tokenize.url2sentences(text)
+        else:
+            self.sentences = self.sent_tokenize.text2sentences(text)
+
+        self.nouns = self.sent_tokenize.get_nouns(self.sentences)
+
+        self.graph_matrix = GraphMatrix()
+        self.sent_graph = self.graph_matrix.build_sent_graph(self.nouns)
+        self.words_graph, self.idx2word = self.graph_matrix.build_words_graph(self.nouns)
+
+        self.rank = Rank()
+        self.sent_rank_idx = self.rank.get_ranks(self.sent_graph)
+        self.sorted_sent_rank_idx = sorted(self.sent_rank_idx, key=lambda k: self.sent_rank_idx[k], reverse=True)
+
+        self.word_rank_idx =  self.rank.get_ranks(self.words_graph)
+        self.sorted_word_rank_idx = sorted(self.word_rank_idx, key=lambda k: self.word_rank_idx[k], reverse=True)
+
+
+    def summarize(self, sent_num=3):
+        summary = []
+        index=[]
+        for idx in self.sorted_sent_rank_idx[:sent_num]:
+            index.append(idx)
+
+        index.sort()
+        for idx in index:
+            summary.append(self.sentences[idx])
+
+        return summary
+
+    def keywords(self, word_num=10):
+        rank = Rank()
+        rank_idx = rank.get_ranks(self.words_graph)
+        sorted_rank_idx = sorted(rank_idx, key=lambda k: rank_idx[k], reverse=True)
+
+        keywords = []
+        index=[]
+        for idx in sorted_rank_idx[:word_num]:
+            index.append(idx)
+
+        #index.sort()
+        for idx in index:
+            keywords.append(self.idx2word[idx])
+
+        return keywords
+
+
+출처: https://excelsior-cjh.tistory.com/93 [EXCELSIOR]
+
+# TextRank 결과 확인
+textrank = TextRank(json_body)
+for row in textrank.summarize(3):
+
+    print(row)
+    print()
+    print('keywords :',textrank.keywords())
